a7f6c132b55898a2b0313325c100fdbfdee287d9
tony-nguyen1 <nguyentoony@gmail.com>
2025-01-26 23:51:52 +0100

    authentification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms'; // Import de ReactiveFormsModule
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ReactiveFormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/authentification/authentification.component.ts
+++ src/app/authentification/authentification.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-authentification',
-  standalone: false,
-
+  standalone: true,
+  imports: [
+    ReactiveFormsModule, // Import du module nécessaire pour les formulaires réactifs
+    CommonModule,
+  ],
   templateUrl: './authentification.component.html',
   styleUrl: './authentification.component.css'
 })
 export class AuthentificationComponent {
+
+  private formBuilder = inject(FormBuilder);
+
+  profileForm: FormGroup = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]]
+  });
+
+  get name() { return this.profileForm.get('name'); }
+  get email() { return this.profileForm.get('email'); }
+
+
   onSubmit(): void {
     console.log("Click")
+  }
+
+  handleSubmit() {
+    alert(
+      this.profileForm.value.name + ' | ' + this.profileForm.value.email
+    );
   }
 }
