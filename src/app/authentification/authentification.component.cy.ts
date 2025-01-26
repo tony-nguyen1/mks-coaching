d9f56298d0834dc9bae9ae659ac068d104a21632
tony-nguyen1 <nguyentoony@gmail.com>
2025-01-26 22:57:25 +0100

    added a route for connexion and preparing for conection

---
+++ src/app/authentification/authentification.component.cy.ts
@@ -0,0 +1,7 @@
+import { AuthentificationComponent } from './authentification.component'
+
+describe('AuthentificationComponent', () => {
+  it('should mount', () => {
+    cy.mount(AuthentificationComponent)
+  })
+})
